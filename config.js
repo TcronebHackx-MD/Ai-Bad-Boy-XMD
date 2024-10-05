0a821bdb21567c00f1cc30e650e293269d579587
Tcroneb Hackx <cartoonboyzfamily@gmail.com>
2024-10-05 08:36:45 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,42 @@
+global.owner = ['62895331520602']  
+global.mods = ['62895331520602'] 
+global.prems = ['62895331520602']
+global.nameowner = 'Tio'
+global.numberowner = '62895331520602' 
+global.mail = 'support@tioprm.eu.org' 
+global.gc = 'https://chat.whatsapp.com/CP5mV2D9IO730HpBgmkuyi'
+global.instagram = 'https://instagram.com/prm2.0'
+global.wm = '© Tio'
+global.wait = '_*Tunggu sedang di proses...*_'
+global.eror = '_*Server Error*_'
+global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
+global.packname = 'Made With'
+global.author = 'Bot WhatsApp'
+global.autobio = false // Set true untuk mengaktifkan autobio
+global.maxwarn = '5' // Peringatan maksimum
+
+//INI WAJIB DI ISI!//
+global.btc = 'YOUR_APIKEY_HERE' 
+//Daftar terlebih dahulu https://api.botcahx.eu.org
+
+//INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
+global.lann = 'YOUR_APIKEY_HERE'
+//Daftar https://api.betabotz.eu.org 
+
+global.APIs = {   
+  btc: 'https://api.botcahx.eu.org',
+  widipe: 'https://widipe.com'
+}
+global.APIKeys = { 
+  'https://api.botcahx.eu.org': 'APIKEY' 
+}
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
